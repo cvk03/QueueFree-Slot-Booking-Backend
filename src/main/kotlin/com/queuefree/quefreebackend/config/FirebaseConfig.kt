983aa6de5506680aeba32552fa6cd17378f03489
Chaitrali Kulkarni <chaitralikulkarni@Chaitralis-MacBook-Air.local>
2026-03-05 20:58:34 +0530

    Updated Firebase config

--- src/main/kotlin/com/queuefree/quefreebackend/config/FirebaseConfig.kt
+++ src/main/kotlin/com/queuefree/quefreebackend/config/FirebaseConfig.kt
@@ -5,6 +5,7 @@ import com.google.firebase.FirebaseOptions
 import com.google.firebase.cloud.FirestoreClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.io.ByteArrayInputStream
 import java.io.InputStream
 
 @Configuration
@@ -13,9 +14,11 @@ class FirebaseConfig {
     @Bean
     fun firestore(): com.google.cloud.firestore.Firestore {
 
-        val serviceAccount: InputStream =
-            javaClass.classLoader.getResourceAsStream("FIREBASE_CREDENTIALS_PATH")
-                ?: throw RuntimeException("Firebase service account not found")
+        val firebaseConfig = System.getenv("FIREBASE_SERVICE_ACCOUNT")
+            ?: throw RuntimeException("Firebase service account not found")
+
+        val serviceAccount = ByteArrayInputStream(firebaseConfig.toByteArray())
+
 
         val options = FirebaseOptions.builder()
             .setCredentials(GoogleCredentials.fromStream(serviceAccount))
